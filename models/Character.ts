df0f0cf3335ba04140cd4fb7f09a629a83e76da5
Chris Wood <wchristopherwood912@gmail.com>
2024-03-11 12:11:34 -0700

    character schema

---
+++ models/Character.ts
@@ -0,0 +1,60 @@
+import { Document, Model, Schema, model } from "mongoose";
+
+export interface ICharacterDocument extends Document {
+  name: string;
+  items: Schema.Types.ObjectId[];
+  health: Number;
+  attack: Number;
+  defense: Number;
+  xp: Number;
+  questsAccepted: Schema.Types.ObjectId[];
+}
+
+// static methods for schemas
+export interface ICharacter extends ICharacterDocument {}
+
+//interface for book model
+interface ICharacterModel extends Model<ICharacterDocument, {}> {}
+
+// Book Schema
+const CharacterSchema = new Schema<ICharacter, ICharacterModel>({
+  name: {
+    type: String,
+    required: true,
+  },
+  items: [
+    {
+      type: Schema.Types.ObjectId,
+      required: false,
+    },
+  ],
+  questsAccepted: [
+    {
+      type: Schema.Types.ObjectId,
+    },
+  ],
+  health: {
+    type: Number,
+    required: true,
+    default: 100,
+  },
+  attack: {
+    type: Number,
+    required: true,
+    default: 10,
+  },
+  defense: {
+    type: Number,
+    required: true,
+    default: 10,
+  },
+  xp: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+const Character = model<ICharacter>("Character", CharacterSchema);
+
+export default Character;
